534dfb7a7ddd134aa6b81dce6395acb1d5d6d689
hp <moemoelwin99@gmail.com>
2019-07-21 09:59:05 +0630

    first commit

---
+++ src/main/java/com/demo/springmvc/SpringMvcApplication.java
@@ -0,0 +1,41 @@
+package com.demo.springmvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    private static Logger logger= LoggerFactory.getLogger(SpringMvcApplication.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
+
+    @Bean @Profile("dev")
+    public CommandLineRunner run(){
+        return args -> {
+            logger.error("Sample Logger message");
+            logger.info("Sample Logger message");
+            logger.warn("Sample Logger message");
+            logger.trace("Sample Logger message");
+            logger.debug("Sample Logger message");
+
+            Arrays.stream(this.applicationContext.getBeanDefinitionNames())
+                    .sorted()
+                    .forEach(System.out::println);
+        };
+    }
+}
---
+++ src/main/java/com/demo/springmvc/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.demo.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(Model model){
+        model.addAttribute ("tagline","Welcome Spring mvc");
+
+        return "welcome";
+    }
+
+}
